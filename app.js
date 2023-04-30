6d85fd785fba7c15913d5313b06451bb2653416b
sediq <you@example.com>
2023-04-30 16:23:26 +0100

    sitehound.json error handling

--- app.js
+++ app.js
@@ -17,73 +17,78 @@ app.listen(
   process.env.PORT,
   console.log("Connector is running at", process.env.PORT)
 );
-var sitehound = JSON.parse(fs.readFileSync("./sitehound.json", "utf-8"));
-function Caller() {
-  // Calles the product.json of the shopify api
-  request({
-    url: process.env.SHOP_NAME,
-    json: true,
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Shopify-Access-Token": process.env.API_TOKEN,
-    },
-  })
-    .then((d) => {
-      if (sitehound != "") {
-        var depleted_product = [];
-        var data = d.products.map((item) => {
-          return {
-            Location: "SEATTLE HUB",
-            Handle: item.handle,
-            Title: item.title,
-            Body: item.body_html,
-            Vendor: item.vendor,
-            Price: item.variants[0].price,
-            Quantity: item.variants[0].inventory_quantity,
-            Type: item.product_type,
-            Status: item.status,
-          };
-        });
-        for (let i = 0; i < data.length; i++) {
-          for (let j = 0; j < sitehound.length; j++) {
-            if (sitehound[j].title == data[i].Title) {
-              const remainder = sitehound[j].quantity - data[i].Quantity;
-              if (remainder > 0) {
-                depleted_product.push({
-                  Location: "SEATTLE HUB",
-                  Title: sitehound[j].title,
-                  Body: sitehound[j].body_html,
-                  Price: sitehound[j].price,
-                  Quantity: remainder,
-                });
+try {
+  const sitehound = fs.readFileSync("sitehound.json");
+  // fs.readFile("./")
+  function Caller() {
+    // Calles the product.json of the shopify api
+    request({
+      url: process.env.SHOP_NAME,
+      json: true,
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Shopify-Access-Token": process.env.API_TOKEN,
+      },
+    })
+      .then((d) => {
+        if (sitehound != "") {
+          var depleted_product = [];
+          var data = d.products.map((item) => {
+            return {
+              Location: "SEATTLE HUB",
+              Handle: item.handle,
+              Title: item.title,
+              Body: item.body_html,
+              Vendor: item.vendor,
+              Price: item.variants[0].price,
+              Quantity: item.variants[0].inventory_quantity,
+              Type: item.product_type,
+              Status: item.status,
+            };
+          });
+          for (let i = 0; i < data.length; i++) {
+            for (let j = 0; j < sitehound.length; j++) {
+              if (sitehound[j].title == data[i].Title) {
+                const remainder = sitehound[j].quantity - data[i].Quantity;
+                if (remainder > 0) {
+                  depleted_product.push({
+                    Location: "SEATTLE HUB",
+                    Title: sitehound[j].title,
+                    Body: sitehound[j].body_html,
+                    Price: sitehound[j].price,
+                    Quantity: remainder,
+                  });
+                  break;
+                }
                 break;
               }
-              break;
             }
           }
-        }
 
-        const csv = json2csv.parse(depleted_product);
-        const params = {
-          Bucket: "updatepool",
-          Key: "products.csv",
-          Body: csv,
-          ContentType: "application/json",
-        };
-        S3.upload(params, (err, _data) => {
-          if (err) console.error(err);
-          else console.log("updated");
-        });
-      } else {
-        console.log("no update");
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+          const csv = json2csv.parse(depleted_product);
+          const params = {
+            Bucket: "updatepool",
+            Key: "products.csv",
+            Body: csv,
+            ContentType: "application/json",
+          };
+          S3.upload(params, (err, _data) => {
+            if (err) console.error(err);
+            else console.log("updated");
+          });
+        } else {
+          console.log("no update");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+} catch (err) {
+  console.log("No update");
 }
 // Caller();
-const interval = 60 * 200 * 5;
+// const interval = 60 * 200 * 5;
 // the caller function will run every 1 minute
-setInterval(Caller, interval);
+// setInterval(Caller, interval);

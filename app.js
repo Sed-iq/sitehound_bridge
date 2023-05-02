226ec7f10d0cc31aa2d468a60acf521fb1373f7d
sediq <you@example.com>
2023-05-02 19:37:31 +0100

    File reading changes

--- app.js
+++ app.js
@@ -17,82 +17,86 @@ app.listen(
   process.env.PORT,
   console.log("Connector is running at", process.env.PORT)
 );
-try {
-  const sitehound = fs.readFileSync("sitehound.json");
-  // fs.readFile("./")
-  function Caller() {
-    // Calles the product.json of the shopify api
-    request({
-      url: process.env.SHOP_NAME,
-      json: true,
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Shopify-Access-Token": process.env.API_TOKEN,
-      },
-    })
-      .then((d) => {
-        if (sitehound != "") {
-          var depleted_product = [];
-          var data = d.products.map((item) => {
-            return {
-              Location: "SEATTLE HUB",
-              Handle: item.handle,
-              Title: item.title,
-              Body: item.body_html,
-              Vendor: item.vendor,
-              Price: item.variants[0].price,
-              Quantity: item.variants[0].inventory_quantity,
-              Type: item.product_type,
-              Status: item.status,
-            };
-          });
-          for (let i = 0; i < data.length; i++) {
-            for (let j = 0; j < sitehound.length; j++) {
-              if (sitehound[j].title == data[i].Title) {
-                const remainder = sitehound[j].quantity - data[i].Quantity;
-                if (remainder > 0) {
-                  depleted_product.push({
-                    Location: "SEATTLE HUB",
-                    Title: sitehound[j].title,
-                    Body: sitehound[j].body_html,
-                    Price: sitehound[j].price,
-                    Quantity: remainder,
-                    To_status: "SOLD",
-                    Status: sitehound[j].status,
-                  });
-                  break;
-                }
+fs.readFile("sitehound.json", (err, data) => {
+  if (err) {
+    console.log("No update");
+  } else {
+    const sitehound = JSON.parse(data);
+    const interval = 60 * 200 * 5;
+    setInterval(() => Caller(sitehound), interval);
+  }
+});
+
+function Caller(sitehound) {
+  // Calles the product.json of the shopify api
+  request({
+    url: process.env.SHOP_NAME,
+    json: true,
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Shopify-Access-Token": process.env.API_TOKEN,
+    },
+  })
+    .then((d) => {
+      if (sitehound != "") {
+        var depleted_product = [];
+        var data = d.products.map((item) => {
+          return {
+            Location: "SEATTLE HUB",
+            Handle: item.handle,
+            Title: item.title,
+            Body: item.body_html,
+            Vendor: item.vendor,
+            Price: item.variants[0].price,
+            Quantity: item.variants[0].inventory_quantity,
+            Type: item.product_type,
+            Status: item.status,
+          };
+        });
+        for (let i = 0; i < data.length; i++) {
+          for (let j = 0; j < sitehound.length; j++) {
+            if (sitehound[j].title == data[i].Title) {
+              const remainder = sitehound[j].quantity - data[i].Quantity;
+              if (remainder > 0) {
+                depleted_product.push({
+                  Location: "SEATTLE HUB",
+                  Title: sitehound[j].title,
+                  Body: sitehound[j].body_html,
+                  Price: sitehound[j].price,
+                  Quantity: remainder,
+                  To_status: "SOLD",
+                  Status: sitehound[j].status,
+                });
                 break;
               }
+              break;
             }
           }
-          if (depleted_product != "") {
-            const csv = json2csv.parse(depleted_product);
-            const params = {
-              Bucket: "updatepool",
-              Key: "products.csv",
-              Body: csv,
-              ContentType: "application/json",
-            };
-            S3.upload(params, (err, _data) => {
-              if (err) console.error(err);
-              else console.log("updated");
-            });
-          } else {
-            console.log("no update");
-            console.log(depleted_product);
-          }
         }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  const interval = 60 * 200 * 5;
-  setInterval(Caller, interval);
-} catch (err) {
-  console.log("No update");
+        if (depleted_product != "") {
+          const csv = json2csv.parse(depleted_product);
+          const params = {
+            Bucket: "updatepool",
+            Key: "products.csv",
+            Body: csv,
+            ContentType: "application/json",
+          };
+          S3.upload(params, (err, _data) => {
+            if (err) console.error(err);
+            else console.log("updated");
+          });
+        } else {
+          console.log("no update");
+          console.log(depleted_product);
+        }
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
+// setInterval(Caller, interval);
+
 // Caller();
 // the caller function will run every 1 minute
